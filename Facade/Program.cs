b9ba1796fe0ea8c5c842b0c495e99aa04ae5d8ba
NoahG <nginburg@new-innov.com>
2021-11-04 12:37:35 -0400

    facade pattern examples

--- Facade/Program.cs
+++ Facade/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection.Metadata.Ecma335;
+using System.Threading.Channels;
 
 namespace Facade
 {
@@ -20,106 +21,191 @@ namespace Facade
             // ex: when eating at a restaurant you don't care where you meal is prepped and cooked in the kitchen, that
             // layer has been abstracted away
 
-            Server server = new Server();
-            Console.WriteLine("Hell, what is your name");
-            var name = Console.ReadLine();
+            //Server server = new Server();
+            //Console.WriteLine("Hell, what is your name");
+            //var name = Console.ReadLine();
 
-            Patron patron = new Patron(name);
+            //Patron patron = new Patron(name);
 
-            Console.WriteLine($"Hello {patron.Name} what app would you like, 1-15?");
-            var appId = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Hello {patron.Name} what app would you like, 1-15?");
+            //var appId = int.Parse(Console.ReadLine());
 
-            Console.WriteLine($"entree? 16-20?");
-            var entreeId = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"entree? 16-20?");
+            //var entreeId = int.Parse(Console.ReadLine());
 
-            Console.WriteLine($"to drink? 21-30");
-            var drinkId = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"to drink? 21-30");
+            //var drinkId = int.Parse(Console.ReadLine());
 
-            server.PlaceOrder(patron, appId, entreeId, drinkId);
+            //server.PlaceOrder(patron, appId, entreeId, drinkId);
 
-            Console.ReadKey();
+            //Console.ReadKey();
+
+            var homeTheaterFacade = new HomeTheaterFacade(new Amplifier(), new DvdPlayer());
+            homeTheaterFacade.WatchMovie("Toy Story");
+            homeTheaterFacade.EndMovie();
         }
+    }
 
-        public class Patron
+    public class Patron
+    {
+        private readonly string _name;
+    
+        public Patron(string name)
         {
-            private readonly string _name;
+            _name = name;
+        }
+    
+        public string Name => _name;
+    }
 
-            public Patron(string name)
+    public class FoodItem
+    {
+        public int DishId;
+    }
+
+    interface IKitchenSection
+    {
+        FoodItem PrepDish(int dishId);
+    }
+
+    public class Order
+    {
+        public FoodItem Appetizer { get; set; }
+        public FoodItem Entree { get; set; }
+        public FoodItem Drink { get; set; }
+    }
+
+    // SUBSYSTEMS:
+    public class ColdPrep : IKitchenSection
+    {
+        public FoodItem PrepDish(int dishId)
+        {
+            return new FoodItem()
             {
-                _name = name;
-            }
-
-            public string Name => _name;
+                DishId = dishId
+            };
         }
+    }
 
-        public class FoodItem
+    public class HotPrep : IKitchenSection
+    {
+        public FoodItem PrepDish(int dishId)
         {
-            public int DishId;
-        }
-
-        interface IKitchenSection
-        {
-            FoodItem PrepDish(int dishId);
-        }
-
-        public class Order
-        {
-            public FoodItem Appetizer { get; set; }
-            public FoodItem Entree { get; set; }
-            public FoodItem Drink { get; set; }
-        }
-
-        // SUBSYSTEMS:
-        public class ColdPrep : IKitchenSection
-        {
-            public FoodItem PrepDish(int dishId)
+            return new FoodItem()
             {
-                return new FoodItem()
-                {
-                    DishId = dishId
-                };
-            }
+                DishId = dishId
+            };
+        }
+    }
+
+    public class Bar : IKitchenSection
+    {
+        public FoodItem PrepDish(int dishId)
+        {
+            return new FoodItem()
+            {
+                DishId = dishId
+            };
+        }
+    }
+
+    //Server FACADE
+    public class Server
+    {
+        private ColdPrep _coldPrep = new ColdPrep();
+        private Bar _bar = new Bar();
+        private HotPrep _hotPrep = new HotPrep();
+
+        public Order PlaceOrder(Patron patron, int coldAppId, int hotEntreeId, int drinkId)
+        {
+            Console.WriteLine($"{patron.Name} places order for cold app {coldAppId.ToString()}, hot entree # {hotEntreeId.ToString()}, and drink $ {drinkId.ToString()}.");
+            Order order = new Order();
+            order.Appetizer = _coldPrep.PrepDish(coldAppId);
+            order.Entree = _hotPrep.PrepDish(hotEntreeId);
+            order.Drink = _bar.PrepDish(drinkId);
+
+            return order;
+        }
+    }
+
+    //Project from gang of four
+    //Home Theater Facade
+    public class HomeTheaterFacade
+    {
+        // composition of subsystem components
+        public Amplifier _amp;
+        public DvdPlayer _dvd;
+    
+        // the constructor takes a dependancy on each component in the subsystem
+        public HomeTheaterFacade(Amplifier amp, DvdPlayer dvd)
+        {
+            _amp = amp;
+            _dvd = dvd;
+        }
+    
+        public void WatchMovie(String movie)
+        {
+            Console.WriteLine($"getting ready to watch {movie}");
+            _amp.On();
+            _amp.SetVolume(5);
+            _dvd.On();
+            _dvd.PlayMovie();
+        }
+    
+        public void EndMovie()
+        {
+            Console.WriteLine($"turning off and shutting down");
+            _amp.Off();
+            _dvd.Stop();
+            _dvd.Eject();
+            _dvd.Off();
+        }
+    }
+
+    public class DvdPlayer
+    {
+        public void On()
+        {
+            Console.WriteLine("dvd player is on");
         }
 
-        public class HotPrep : IKitchenSection
+        public void PlayMovie()
         {
-            public FoodItem PrepDish(int dishId)
-            {
-                return new FoodItem()
-                {
-                    DishId = dishId
-                };
-            }
+            Console.WriteLine("movie playing");
         }
 
-        public class Bar : IKitchenSection
+        public void Stop()
         {
-            public FoodItem PrepDish(int dishId)
-            {
-                return new FoodItem()
-                {
-                    DishId = dishId
-                };
-            }
+            Console.WriteLine("stropping movie");
         }
 
-        //FACADE
-        public class Server
+        public void Eject()
         {
-            private ColdPrep _coldPrep = new ColdPrep();
-            private Bar _bar = new Bar();
-            private HotPrep _hotPrep = new HotPrep();
+            Console.WriteLine("get this dvd out of here");
+        }
 
-            public Order PlaceOrder(Patron patron, int coldAppId, int hotEntreeId, int drinkId)
-            {
-                Console.WriteLine($"{patron.Name} places order for cold app {coldAppId.ToString()}, hot entree # {hotEntreeId.ToString()}, and drink $ {drinkId.ToString()}.");
-                Order order = new Order();
-                order.Appetizer = _coldPrep.PrepDish(coldAppId);
-                order.Entree = _hotPrep.PrepDish(hotEntreeId);
-                order.Drink = _bar.PrepDish(drinkId);
+        public void Off()
+        {
+            Console.WriteLine("dvd player turned off");
+        }
 
-                return order;
-            }
+    }
+
+    public class Amplifier
+    {
+        public void On()
+        {
+            Console.WriteLine("turning amp on");
+        }
+
+        public void SetVolume(int vol)
+        {
+            Console.WriteLine($"volume set to {vol}");
+        }
+
+        public void Off()
+        {
+            Console.WriteLine("amp turned off");
         }
 
     }
